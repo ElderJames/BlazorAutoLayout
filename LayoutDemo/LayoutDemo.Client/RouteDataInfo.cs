80214565bc7fd1d40b35759ba1cd0d42ecee820c
James <yangshunjie@yesmro.cn>
2023-10-20 23:32:03 +0800

    Add project files.

---
+++ LayoutDemo/LayoutDemo.Client/RouteDataInfo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json.Serialization;
+
+namespace LayoutDemo.Client
+{
+    public class RouteDataInfo
+    {
+        public string PageTypeName { get; set; }
+        public IReadOnlyDictionary<string, object?> RouteValues { get; set; }
+
+        public RouteDataInfo() { }
+
+      
+        public RouteDataInfo(RouteData routeData)
+        {
+            PageTypeName = routeData.PageType.FullName;
+            RouteValues = routeData.RouteValues;
+        }
+
+        public RenderFragment RenderBody()
+        {
+            return builder =>
+            {
+                var pageType = Type.GetType(PageTypeName);
+                if (pageType == null)
+                {
+                    return;
+                }
+
+                builder.OpenComponent(0, pageType);
+                foreach (var routeValue in RouteValues)
+                {
+                    builder.AddAttribute(1, routeValue.Key, routeValue.Value);
+                }
+                builder.CloseComponent();
+            };
+        }
+    }
+}
