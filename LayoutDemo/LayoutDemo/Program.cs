b4e8911f5891647729c76c4d34eda46c37429973
James Yeung <shunjiey@hotmail.com>
2023-12-06 00:24:52 +0800

    multiple app

--- LayoutDemo/LayoutDemo/Program.cs
+++ LayoutDemo/LayoutDemo/Program.cs
@@ -1,5 +1,14 @@
+using LayoutDemo.Client;
 using LayoutDemo.Client.Pages;
 using LayoutDemo.Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +25,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+
+
+        app.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
+            string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
+    
 }
 else
 {
@@ -29,9 +43,33 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Admin).Assembly);
+app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/Admin"), second =>
+{
+    second.UseStaticFiles();
+    second.UseStaticFiles("/Admin");
+
+    second.UseRouting();
+    second.UseAntiforgery();
+    second.UseEndpoints(endpoints =>
+    {
+        endpoints.MapRazorComponents<App2>()
+         .AddInteractiveWebAssemblyRenderMode();
+    });
+});
+
+app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/Home"), first =>
+{
+
+    first.UseStaticFiles();
+    first.UseStaticFiles("/Home");
+    first.UseRouting();
+    first.UseEndpoints(endpoints =>
+    {
+        endpoints.MapRazorComponents<App>();
+    });
+});
+
+app.MapRazorComponents<App>().AddInteractiveWebAssemblyRenderMode();
 
 app.Run();
+
